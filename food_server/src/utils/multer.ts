06be6e4ea2c1551b10d90adc639b0f29d1c03d03
alianu23 <142651654+alianu23@users.noreply.github.com>
2024-04-06 14:35:00 +0800

    multer des

--- food_server/src/utils/multer.ts
+++ food_server/src/utils/multer.ts
@@ -2,12 +2,13 @@ import multer from "multer";
 import path from "path";
 
 const storage = multer.diskStorage({
-  destination: (_req, _file, cb) => {
-    console.log("path", path.join(__dirname, "../uploads"));
-    cb(null, path.join(__dirname, "../uploads"));
+  destination: function (req, file, cb) {
+    console.log("d", path.join(__dirname, "../uploads"));
+
+    cb(null, path.join(__dirname, "../uploads/"));
   },
-  filename: (req, _file, cb) => {
-    cb(null, _file.originalname);
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
   },
 });
 
